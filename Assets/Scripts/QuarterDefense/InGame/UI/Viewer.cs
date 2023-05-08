03846df8021a73cbd5cf7aa784942444f85d4acd
Raycast <hyeok0215@naver.com>
2023-05-09 04:53:58 +0900

    인게임 UI에 사용하는 골드, 웨이브 viewer 클래스 추가

---
+++ Assets/Scripts/QuarterDefense/InGame/UI/Viewer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace QuarterDefense.InGame.UI
+{
+    // Scripted by Raycast
+    // 2023. 05. 09
+    // UI에 표시할 Viewer의 상위 클래스입니다.
+    
+    public abstract class Viewer : MonoBehaviour
+    {
+        public abstract void Set(int delta);
+    }
+}
---
+++ Assets/Scripts/QuarterDefense/InGame/UI/WaveViewer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QuarterDefense.InGame.UI
+{
+    // Scripted by Raycast
+    // 2023. 05. 09
+    // Wave를 UI에 표시하는 클래스입니다.
+    
+    public class WaveViewer : Viewer
+    {
+        [SerializeField] private Text waveText = null;
+
+        private int _currentWave = 0;
+        
+        public override void Set(int delta)
+        {
+            _currentWave += delta;
+            
+            waveText.text = $"{_currentWave}";
+        }
+    }
+}
