3576ceaf53f5cf1eff9641750b24f5064fe5f6dd
Raycast <hyeok0215@naver.com>
2023-05-20 07:17:19 +0900

    플레이어 수정

--- Assets/Scripts/QuarterDefense/InGame/Player/Player.cs
+++ Assets/Scripts/QuarterDefense/InGame/Player/Player.cs
@@ -11,6 +11,8 @@ namespace QuarterDefense.InGame.Player
     {
         [SerializeField] private BulletController bulletController = null;
 
+        [SerializeField] private Animator animator = null;
+        
         private List<Enemy> _enemyList = new List<Enemy>();
 
         private Coroutine _coroutine = null;
@@ -19,9 +21,23 @@ namespace QuarterDefense.InGame.Player
         
         private void Start()
         {
-            //StartCoroutine(Fire());
+            // StartCoroutine(Attack());
         }
 
+
+        private IEnumerator Attack()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(2.0f);
+                
+                animator.Play("Player_Attack", 0, 0.0f);
+            }
+        }
+        
+        
+        
+        
         public void SetEnemyList(List<Enemy> enemyList)
         {
             _enemyList = enemyList;
@@ -34,53 +50,16 @@ namespace QuarterDefense.InGame.Player
         {
             while (true)
             {
-                yield return new WaitForSeconds(0.2f);
-                
-                if(preTarget) preTarget.SetSelect = false;
+                yield return new WaitForSeconds(1.0f);
                 
                 var toList = _enemyList.OrderBy(
                     x => Vector3.Distance(transform.position, x.transform.position)).ToList();
 
                 preTarget = toList[0];
                 
-                preTarget.SetSelect = true;
-
                 preTarget.Damage(1);
             }
         }
-
-        // private void Update()
-        // {
-        //     RaycastHit hit;
-        //
-        //     Debug.DrawRay(transform.position, Vector3.left * 20.0f, Color.red);
-        //     
-        //     if (Physics.Raycast(transform.position, transform.forward, out hit, 20.0f))
-        //     {
-        //         Debug.Log("????");
-        //         
-        //         hit.collider.GetComponent<Enemy>().SetSelect = true;
-        //     }
-        // }
-        //
-        //
-        // private IEnumerator Fire()
-        // {
-        //     
-        //     
-        //     yield return new WaitForSeconds(3.0f);
-        //     
-        //     while (true)
-        //     {
-        //         yield return new WaitForSeconds(1.0f);
-        //
-        //         BaseBullet bullet = bulletController.FindUsableBullet();
-        //
-        //         if (!bullet) continue;
-        //         
-        //         bullet.Spawned();
-        //     }
-        // }
     }
 }
 
