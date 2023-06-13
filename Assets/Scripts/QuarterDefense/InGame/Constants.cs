c696c51dedb8be542c03ac6e610cf923e7d18c4e
Raycast <hyeok0215@naver.com>
2023-06-13 22:06:27 +0900

    상수 추가

---
+++ Assets/Scripts/QuarterDefense/InGame/Constants.cs
@@ -0,0 +1,12 @@
+namespace QuarterDefense.InGame
+{
+    // Scripted by Raycast
+    // 2023. 06. 11
+    // 공통으로 사용할 상수들을 모아둔 클래스입니다.
+    
+    public static class Constants
+    {
+        public const int SpawnCost = 10;
+        public const int RankUpCharacterCount = 10;
+    }
+}
